b5cdc5a310a4c6df7608c58e2a87a3c19beccf04
julio <julio@DESKTOP-F4GVUGB>
2021-10-19 17:32:55 -0500

    Agregar archivos de proyecto.

---
+++ appconsolearreglo.cs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace appconsolearreglo.cs
+{
+    class Program
+    {
+        private static string nombre, edad, cita;
+        
+        static void Main(string[] args)
+        {
+            persona[] arreglo = new persona[2];
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.WriteLine("¿cual es tu nombre?");
+                nombre = Console.ReadLine();
+                Console.WriteLine("¿cual es tu edad?");
+                edad = Console.ReadLine();
+                Console.WriteLine("cuando es tu cita?");
+                cita = Console.ReadLine();
+                arreglo[i] = new persona();
+                arreglo[i].Nombre = nombre;
+                arreglo[i].Edad = edad;
+                arreglo[i].Cita = cita;
+
+            }
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.WriteLine((i + 1) + arreglo[i].ToString());
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ appconsolearreglo.cs/persona.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace appconsolearreglo.cs
+{
+    class persona
+    {
+        private string nombre;
+        private String cita;
+        private String edad;
+
+        public string Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+
+        public string Cita
+        {
+            get { return cita; }
+            set { cita = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public persona()
+        {
+            Nombre = "";
+            Cita = null;
+            edad = "";
+        }
+        public persona(string nombre, string edad , string cita)
+        {
+            this.nombre = Nombre;
+            this.cita = Cita;
+            this.edad = Edad;
+
+        }
+        public override string ToString()
+        {
+            return nombre + " " + cita + " " + edad + " ";
+        }
+
+
+    }
+    
+}
